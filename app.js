69a70b49b2a792e9e752e56e77ae9d99a5aee964
Samuel Pink <samuel.pink@outlook.com>
2019-09-19 19:04:01 +0100

    Added UpdateTo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport-jwt');
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
 
 const mongoCon = 'mongodb+srv://sam:4vXCVrR99Ctz37ec@cluster-iualb.mongodb.net/test?retryWrites=true&w=majority';
 mongoose.connect(mongoCon, {
@@ -26,6 +26,9 @@ const users = require('./routes/users');
 const port = 3000;
 
 app.use(cors());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
